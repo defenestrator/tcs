a6e919918ac859415a79d1f4ec654a6d2ed63ab4
The Defenestrator <529446+defenestrator@users.noreply.github.com>
2020-04-01 13:23:14 -0600

    @jasonmccreary rocks my socks, thanks for blueprint. I tagged someone else,  absent-fingeredly in my big fat commits, hahah

--- database/seeds/EverythingSeeder.php
+++ database/seeds/EverythingSeeder.php
@@ -24,7 +24,6 @@ class EverythingSeeder extends Seeder
         factory(App\Order::class, 10)->create();
         factory(App\Product::class, 10)->create();
         factory(App\ProductCategory::class, 10)->create();
-        // factory(App\Profile::class, 10)->create();
         factory(App\Shipment::class, 10)->create();
         factory(App\ShippingAddress::class, 10)->create();
         factory(App\ShippingCarrier::class, 10)->create();
